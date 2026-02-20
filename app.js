9bfbff2c49c411c2ca08ad8dbd861491f15d7d42
hafeez-07 <hafeezmohammad689@gmail.com>
2026-02-20 16:17:02 +0530

    feat : Add profile update,improve styles

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ import { fileURLToPath } from "url";
 import { match } from "assert";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+import uploads from "./config/multerConfig.js";
 
 app.set("view engine", "ejs");
 
@@ -153,8 +154,10 @@ app.post("/post", protect, async (req, res) => {
 });
 
 app.get("/blogs", protect, async (req, res) => {
+  const userId = req.user.id;
+  const user = await User.findById(userId);
   const posts = await Post.find().populate("user", " _id username profilePic");
-  res.render("blogs", { posts });
+  res.render("blogs", { posts, user });
 });
 
 app.get("/post/:id", protect, async (req, res) => {
@@ -226,13 +229,94 @@ app.get("/like/:id", protect, async (req, res) => {
     await post.save();
 
     res.redirect(req.get("Referer") || "/blogs");
-   
   } catch (err) {
     res.status(500).json({
       error: err.message,
     });
   }
 });
+
+app.get("/profile", protect, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    res.render("profile", { user });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.get("/profile/upload", protect, (req, res) => {
+  try {
+    res.render("profileUpload");
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.post(
+  "/profile/uploadImage",
+  protect,
+  uploads.single("profilePic"),
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+      if (!userId) {
+        return res.status(400).json({
+          error: "please log in",
+        });
+      }
+      if (!req.file) {
+        return res.status(400).json({
+          error: "No file uploaded",
+        });
+      }
+      const user = await User.updateOne(
+        { _id: userId },
+        {
+          profilePic: `/images/uploads/${req.file.filename}`,
+        },
+      );
+
+      res.redirect("/profile");
+    } catch (err) {
+      res.status(500).json({
+        error: err.message,
+      });
+    }
+  },
+);
+
+app.post(
+  "/profile/update",
+  protect,
+
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+
+      const { username, name, email, age } = req.body;
+
+      const updatedUser = await User.updateOne(
+        { _id: userId },
+        {
+          username,
+          name,
+          email,
+          age,
+        },
+      );
+
+      res.redirect("/profile");
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App running in localhost:3000");
--- config/multerConfig.js
+++ config/multerConfig.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "./public/images/uploads"));
+    cb(null, path.join(process.cwd(), "./public/images/uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
